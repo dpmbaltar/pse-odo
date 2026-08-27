055abcba9d862cc772c6040c8eea1a942b970280
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2026-08-27 11:05:13 -0300

    Agregar OP_ACK

--- main/hodor.h
+++ main/hodor.h
@@ -9,6 +9,7 @@
 #define AD_MASK  0xFC /**< Máscara del offset del byte del estado */
 #define OP_MASK  0x03 /**< Máscara de la operación sobre el estado */
 
+#define OP_ACK   0
 #define OP_READ  1    /**< Operación de lectura */
 #define OP_WRITE 2    /**< Operación de escritura */
 
@@ -16,7 +17,7 @@
 
 #define MSG_HEAD(op, ad) \
     (((((uint8_t)(ad)) << 2) & AD_MASK) | (((uint8_t)(op)) & OP_MASK))
-#define MSG_AD(h) \
+#define MSG_ADDR(h) \
     ((((uint8_t)(h)) & AD_MASK) >> 2)
 #define MSG_OP(h) \
     (((uint8_t)(h)) & OP_MASK)
@@ -60,6 +61,8 @@ typedef struct __attribute__((packed))
     uint8_t head; /**< Encabezado del mensaje */
 
     union {
+        int16_t body;
+
         int16_t lenc_steps;
         int16_t renc_steps;
         int16_t lmotor_pwm;
@@ -78,6 +81,6 @@ void hodor_init(void);
 void hodor_st_get(hodor_ad_t ad, int16_t *val);
 void hodor_st_set(hodor_ad_t ad, int16_t val);
 void hodor_msg_send(hodor_msg_t *msg);
-void hodor_msg_recv(hodor_msg_t *msg);
+int  hodor_msg_recv(hodor_msg_t *msg);
 
 #endif /* HODOR_H */
